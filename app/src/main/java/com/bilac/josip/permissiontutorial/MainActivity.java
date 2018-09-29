501a63de002f9921f7281ffc662b95fb95221aa3
bilacc <bilacjosip@gmail.com>
2018-09-29 12:48:45 +0200

    Initial commit

---
+++ app/src/main/java/com/bilac/josip/permissiontutorial/MainActivity.java
@@ -0,0 +1,94 @@
+package com.bilac.josip.permissiontutorial;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * Based on:
+ * https://github.com/probelalkhan/android-marshmallow-permissions-example/blob/master/app/src/main/java/net/simplifiedcoding/runtimepermission/MainActivity.java
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private final int STORAGE_PERMISSION_CODE = 101;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnPermission)
+    public void askForPermission(){
+        //First checking if the app is already having the permission
+        if(isReadStorageAllowed()){
+            //If permission is already having then showing the toast
+            Toast.makeText(MainActivity.this
+                    ,"You already have the permission",Toast.LENGTH_LONG).show();
+            //Existing the method with return
+            return;
+        }
+
+        //If the app has not the permission then asking for the permission
+        requestStoragePermission();
+    }
+
+    //We are calling this method to check the permission status
+    private boolean isReadStorageAllowed() {
+        //Getting the permission status
+        int result = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        //If permission is granted returning true
+        if (result == PackageManager.PERMISSION_GRANTED)
+            return true;
+
+        //If permission is not granted returning false
+        return false;
+    }
+
+    //Requesting permission
+    private void requestStoragePermission(){
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)){
+            //If the user has denied the permission previously your code will come to this block
+            //Here you can explain why you need this permission
+            //Explain here why you need this permission
+        }
+
+        //And finally ask for the permission
+        ActivityCompat.requestPermissions(this,new String[]{
+                Manifest.permission.READ_EXTERNAL_STORAGE},STORAGE_PERMISSION_CODE);
+    }
+
+    //This method will be called when the user will tap on allow or deny
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+
+        //Checking the request code of our request
+        if(requestCode == STORAGE_PERMISSION_CODE){
+
+            //If permission is granted
+            if(grantResults.length >0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                //Displaying a toast
+                Toast.makeText(this,"Permission granted now you can read the storage",Toast.LENGTH_LONG).show();
+            }else{
+                //Displaying another toast if permission is not granted
+                Toast.makeText(this,"Oops you just denied the permission", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
